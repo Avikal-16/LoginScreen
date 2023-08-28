6ce39b4b2d1f249ee76f814cbb1e3366f6d110d5
Avikal Gupta <avikal.gupta2003@gmail.com>
2023-08-28 18:10:57 +0530

    Initial commit

---
+++ app/src/main/java/LoginScreen.kt
@@ -0,0 +1,59 @@
+package com.example.login_act
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login", style = MaterialTheme.typography.h4)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        var username by remember { mutableStateOf(TextFieldValue()) }
+        var password by remember { mutableStateOf(TextFieldValue()) }
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Handle login button click */ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/example/login_act/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.login_act
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.login_act.ui.theme.Login_actTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreen()
+        }
+    }
+}
+
